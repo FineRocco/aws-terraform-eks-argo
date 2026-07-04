336fcfd7a0a061cb781f6df82d507a03988caa83
Denis Ungureanu <l3ti.me20@gmail.com>
2026-07-04 13:05:30 +0100

    Update GitHub Actions role policy to reference the correct repository for EKS Argo

--- tf-bootstrap-backend/main.tf
+++ tf-bootstrap-backend/main.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
           }
           StringLike = {
-            "token.actions.githubusercontent.com:sub" = "repo:FineRocco/aws-terraform-ec2-ssm:*"
+            "token.actions.githubusercontent.com:sub" = "repo:FineRocco/aws-terraform-eks-argo:*"
           }
         }
       }
